8627f67a11645d5a23bbdd3b7e95077796485025
Joe-Thompson <bmo3696@gmail.com>
2020-08-25 19:35:34 -0400

    working on BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,12 +19,46 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        #  inserting with single node
+        #       compare target value to node.value
+        #       if target > node.value
+        #           if node.right is None then create new node
+        #           else node.right is not None:
+        #               compare target value to node.value
+        #       else target is < node.value
+        #           if node.left is None then creat new node
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        if self.value < target:
+            right_node = self.right
+            while self.right.right is not None:
+                if right_node == target:
+                    return True
+                elif right_node.right is None:
+                    return False
+            right_node = right_node.right
+        if self.value > target:
+            left_node = self.left
+            while self.left.left is not None:
+                if left_node == target:
+                    return True
+                elif left_node.left is None:
+                    return False
+            left_node = left_node.left
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -60,6 +96,7 @@ class BSTNode:
     def post_order_dft(self):
         pass
 
+
 """
 This code is necessary for testing the `print` methods
 """
@@ -76,10 +113,10 @@ bst.insert(2)
 bst.bft_print()
 bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()
